7d7abf52b011b7926213a6b6704a1e31bf6ffa6b
unknown <abdulazizapu@gmail.com>
2023-05-30 14:23:36 +0600

    test project

---
+++ src/HelloNpm/HelloNpm.jsx
@@ -0,0 +1,9 @@
+import * as React from 'react'
+
+export default function HelloNpm () {
+    return (
+        <div>
+            <p>Hello Npm, this is my first published package on Npm</p>
+        </div>
+    )
+}
